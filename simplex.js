733371d5ab04c3e29cc796d97e302f189196673c
Victor Turturika <v.turturika@gmail.com>
2015-10-24 15:20:11 +0300

    First commint. Empty simplex library

---
+++ simplex.js
@@ -0,0 +1,9 @@
+(function(){
+
+    var simplex = {},
+        a = [],          /* array of conditions coefficients */
+        f = [],          /* array of target function coefficients */
+        M = 1000000;     /* "very big number" for method of artificial basis */
+    
+    window.simplex = simplex;
+}());
